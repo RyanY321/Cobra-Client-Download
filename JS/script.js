926196162c6bfd8ab4f811cc4b6f82ace4b413cf
Ryan Youngjohn <ryanyoungjohn321@gmail.com>
2022-11-04 23:03:53 -0400

    install / construction

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,27 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+var seconds = 7; 
+var foo; 
+
+function redirect() {
+    document.location.href = 'https://www.cobraclient.gq/';
+}
+
+function updateSecs() {
+    document.getElementById("seconds").innerHTML = seconds;
+    seconds--;
+    if (seconds == -1) {
+        clearInterval(foo);
+        redirect();
+    }
+}
+
+function countdownTimer() {
+    foo = setInterval(function () {
+        updateSecs()
+    }, 1000);
+}
+
+countdownTimer();
