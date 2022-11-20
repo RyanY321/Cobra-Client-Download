a8843327956153d049d5bb8e30bc3840158c6568
Ryan Youngjohn <ryanyoungjohn321@gmail.com>
2022-11-19 22:49:45 -0500

    bar update

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,8 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
